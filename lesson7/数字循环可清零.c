1d4893cbd622e5b2064b1726eb8213c5b3db3e92
MrZhang-12138 <=>
2020-06-30 21:53:21 +0800

    第七次作业（3）

---
+++ lesson7/数字循环可清零.c
@@ -0,0 +1,50 @@
+#define IN1 4
+#define IN2 5
+#define IN3 6
+#define IN4 7
+
+#define S 8
+#define LT 9
+#define BT 10
+
+byte income=0;
+int i=0;
+int pinInterrupt=2;
+
+void myfun()
+{
+  income=-1;
+  i=0;
+}
+
+void setup()
+{
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  
+  pinMode(S, OUTPUT);
+  pinMode(LT,OUTPUT);
+  pinMode(BT,OUTPUT);
+  
+  digitalWrite(LT,HIGH);
+  digitalWrite(BT,HIGH);
+  digitalWrite(S,LOW);
+  
+  attachInterrupt(digitalPinToInterrupt(pinInterrupt),myfun,CHANGE);
+}
+
+void loop()
+{
+  for(i=0;i<9;i++)
+  {
+    income=income-'0';
+    digitalWrite(4,income&0x1);
+    digitalWrite(5,(income>>1)&0x1);
+    digitalWrite(6,(income>>2)&0x1);
+    digitalWrite(7,(income>>3)&0x1);
+    delay(1000);
+    income++;
+  }
+}
